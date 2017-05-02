e90340c680e0b9e40ceea05eabecee13347524f0
Anton Ivanov <anton.al.ivanov@gmail.com>
2017-05-02 23:14:26 +0300

    Elm's own 'map' and 'reduce' wrapped as 'Underscore' module exports.

--- src/Underscore.elm
+++ src/Underscore.elm
@@ -1,7 +1,23 @@
-module Underscore exposing (map)
+module Underscore exposing (map, reduce)
 
-map : List a -> (a -> b) -> List b
-map list f =
-  case list of
-    [] -> []
-    head::tail -> (f head)::(map tail f)
+{-| Port to Elm of Underscore 1.8.3 functions.
+
+@docs map
+@docs reduce
+-}
+
+import List exposing (map, foldl)
+
+{-| Transforms a given list by applying the provided element transformation function to every element.
+
+    map [1, 2, 3] (\x -> x > 1) == [False, True, True]
+-}
+map : (a -> b) -> List a -> List b
+map = List.map
+
+{-| Reduces the list given the initial value and the reduction step definition.
+
+    reduce (\s x -> s + x) 0 [1, 2, 3] == 6
+-}
+reduce : (a -> b -> b) -> b -> List a -> b
+reduce = List.foldl
--- tests/Tests.elm
+++ tests/Tests.elm
@@ -1,6 +1,6 @@
 module Tests exposing (..)
 
-import Underscore exposing (map)
+import Underscore exposing (map, reduce)
 
 import Test exposing (..)
 import Expect
@@ -9,11 +9,19 @@ all : Test
 all =
     describe "Underscore.elm"
         [ describe "map"
-            [ test "non-empty list" <|
-                \() ->
-                    Expect.equal [2, 4, 6] (Underscore.map [1, 2, 3] (\x -> 2 * x))
+          [ test "non-empty list" <|
+            \() ->
+              Expect.equal [2, 4, 6] (Underscore.map (\x -> 2 * x) [1, 2, 3])
             , test "empty list" <|
-                \() ->
-                    Expect.equal [] (Underscore.map [] (\x -> 2 * x))
+              \() ->
+                Expect.equal [] (Underscore.map (\x -> 2 * x) [])
             ]
+        , describe "reduce"
+          [ test "non-empty list" <|
+            \() ->
+              Expect.equal 6 (reduce (\s x -> s + x) 0 [1, 2, 3])
+            , test "empty list" <|
+              \() ->
+                Expect.equal 0 (reduce (\s x -> s + x) 0 [])
+          ]
         ]
